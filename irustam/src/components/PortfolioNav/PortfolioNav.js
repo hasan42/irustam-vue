78c6b7cd7de45704bbe4f0f174931c1d37a726d5
rpokosov <hasan83842@gmail.com>
2019-06-06 15:27:08 +0600

    nav add active class

--- irustam/src/components/PortfolioNav/PortfolioNav.js
+++ irustam/src/components/PortfolioNav/PortfolioNav.js
@@ -3,4 +3,15 @@ import axios from 'axios'
 export default {
     name: 'PortfolioNav',
     props: ['companys', 'selectfn'],
+    data: function () {
+        return {
+            active: 'all'
+        }
+    },
+    methods: {
+        setActive: function (tag) {
+            this.active = tag;
+            console.log(this.active)
+        }
+    }
 }
