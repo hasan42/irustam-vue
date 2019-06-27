5cae8d8885ef0a12ed546fe33c6d26b698302d3c
rpokosov <hasan83842@gmail.com>
2019-06-27 18:35:30 +0600

    modal is work

--- irustam/src/components/Header/Header.js
+++ irustam/src/components/Header/Header.js
@@ -1,3 +1,14 @@
+import Modal from '../Modal/Modal.vue'
+import EventBus from '../../eventBus';
+
 export default {
-  name: 'Header'
+    name: 'Header',
+    components: {
+        Modal
+    },
+    methods: {
+        showContacts: function () {
+            EventBus.$emit("openModal");
+        }
+    }
 }
--- irustam/src/components/Modal/Modal.js
+++ irustam/src/components/Modal/Modal.js
@@ -1,3 +1,22 @@
+import EventBus from '../../eventBus';
+
 export default {
-    name: 'Modal'
+    name: 'Modal',
+    data() {
+        return {
+            visible: false
+        };
+    },
+    created() {
+        EventBus.$on("openModal", () => {
+            this.visible = true;
+            document.body.classList.add('modal-open');
+        });
+    },
+    methods: {
+        close: function () {
+            this.visible = false;
+            document.body.classList.remove('modal-open');
+        }
+    }
 }
